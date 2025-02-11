b88929c464cf58cf65561f7bfabe5aeb59c9cd4c
Vladimir <flin007@inbox.ru>
2025-02-11 03:24:50 +0300

    Telegram webb app @twa-dev/sdk & tailwind

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
         <h1>Hello world!</h1>
+        <div v-if="$tg.initDataUnsafe">
+            <h2>User Data:</h2>
+            <pre>{{ JSON.stringify($tg.initDataUnsafe, null, 2) }}</pre>
+        </div>
+        <div v-else>
+            <p>No user data received</p>
+        </div>
     </div>
 </template>
+
+<script setup>
+import {getCurrentInstance} from 'vue'
+const {$tg} = getCurrentInstance().appContext.config.globalProperties
+
+
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,30 @@
 import './bootstrap';
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
+import "../css/app.css";
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+//Телеграм апп
+import TelegramWebApp from '@twa-dev/sdk';
 
 createInertiaApp({
     resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
         return pages[`./Pages/${name}.vue`]
     },
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el)
+    setup({el, App, props, plugin}) {
+        const VueApp = createApp({render: () => h(App, props)});
+        // Создаем глобальный объект TelegramWebApp
+        const tg = TelegramWebApp;
+        // Добавляем TelegramWebApp как глобальную свойство
+        VueApp.config.globalProperties.$tg = tg;
+        // Вызываем tg.ready() для инициализации
+        tg.ready();
+
+        // Остальные плагины и компоненты
+        VueApp.use(plugin)
+            .mount(el);
+    },
+    progress: {
+        color: "#2563eb",
+        showSpinner: true,
     },
 })
