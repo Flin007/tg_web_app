eb2176a9c6d0b3fc6f06b823a060255abf151b93
Vladimir <flin007@inbox.ru>
2025-02-13 05:18:12 +0300

    Правки в хедер + линк на разработчика

--- resources/js/Components/Home/Header.vue
+++ resources/js/Components/Home/Header.vue
@@ -1,22 +1,28 @@
 <template>
-    <header class="p-4 flex items-center space-x-4 border-b border-gray-300">
-        <img
-            v-if="user && user.photo_url"
-            :src="user.photo_url"
-            alt="User avatar"
-            class="w-10 h-10 rounded-full"
-        />
-        <span v-else class="w-10 h-10 rounded-full inline-flex items-center justify-center bg-gray-300">
-      <!-- Замените на иконку по умолчанию или текст -->
-      <span class="text-gray-700 text-xl">?</span>
-    </span>
+    <header class="p-4 flex justify-between items-center border-b border-gray-300 bg-white">
+        <!-- Профиль юзера -->
+        <div class="flex items-center">
+            <!-- Аватар, если нет ? -->
+            <img
+                v-if="user && user.photo_url"
+                :src="user.photo_url"
+                alt="User avatar"
+                class="w-10 h-10 rounded-full"
+            />
+            <span v-else class="w-10 h-10 rounded-full inline-flex items-center justify-center bg-gray-300">
+                <span class="text-gray-700 text-xl">?</span>
+            </span>
 
-        <div>
-            <p v-if="user" class="text-sm font-bold">
-                {{ user.first_name ? `${user.first_name} ${user.last_name || ''}` : user.username }}
-            </p>
-            <p v-else class="text-sm font-bold">Гость</p>
+            <!-- ФИ или username -->
+            <div class="ml-2.5">
+                <p v-if="user" class="text-sm font-bold">
+                    {{ user.first_name ? `${user.first_name} ${user.last_name || ''}` : user.username }}
+                </p>
+                <p v-else class="text-sm font-bold">Гость</p>
+            </div>
         </div>
+        <!-- ссылка на разраба -->
+        <a target="_blank" href="https://t.me/notcollector" class="font-light text-sm text-blue-400">.dev</a>
     </header>
 </template>
 
