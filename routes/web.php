e022f87b520768be8f4b045e87347f2a8bbae1ca
Vladimir <flin007@inbox.ru>
2025-02-13 05:19:27 +0300

    Убрал ненужный контролле и лишние роуты

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Http\Controllers\testcontroller;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WebhookController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 //Для принятия хуков от бота
 Route::post('/webhook', WebhookController::class);
-Route::get('/test', testcontroller::class);
 
-Route::get('/', function () {
-    return Inertia::render('Home');
-});
+/** HOMEPAGE */
+Route::get('/', [HomeController::class, 'index'])->name('home');
