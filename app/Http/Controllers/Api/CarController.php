9666eef2ca5b48eddc465b94dd1bae9f0aa00384
Vladimir <flin007@inbox.ru>
2025-02-18 04:49:26 +0300

    api для получения машин

---
+++ app/Http/Controllers/Api/CarController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Car;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $cars = Car::with([
+            'brand',
+            'model',
+            'city',
+            'colors',
+            'photos' => function ($query) {
+                $query->orderBy('sort_order');
+            }
+        ])
+            ->filter(request(Car::AVAILABLE_FILTERS))
+            ->where('is_available', true)
+            ->paginate(1); //TODO: поставить норм значение после тестов
+
+        return response()->json($cars);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\CarController;
 use App\Http\Controllers\ContentController;
 use Illuminate\Support\Facades\Route;
 
 //Получение динамических vue компонентов
 Route::get('/content/home-title', [ContentController::class, 'getHomeTitle']);
+
+//Получение автомобилей
+Route::get('/cars', [CarController::class, 'index']);
