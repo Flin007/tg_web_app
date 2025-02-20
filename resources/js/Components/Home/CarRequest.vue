f256b8c6aff8e4bf24b71924fa80afd741973491
Vladimir <flin007@inbox.ru>
2025-02-20 21:53:52 +0300

    Стили для выключенной кнопки

--- resources/js/Components/Home/CarRequest.vue
+++ resources/js/Components/Home/CarRequest.vue
@@ -33,7 +33,7 @@
                                         <!-- Кнопка назад -->
                                         <button disabled
                                                 type="submit"
-                                                class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                                class="disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                                                 @click=""
                                         >
                                             Назад
