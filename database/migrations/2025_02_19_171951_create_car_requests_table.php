acadcb66c2bec3b02d61f3659154b601ce0a0862
Vladimir <flin007@inbox.ru>
2025-02-20 05:05:30 +0300

    Изменяем определение внешнего ключа в таблице car_requests, чтобы он ссылался на user_id, а не на id в таблице telegram_users

--- database/migrations/2025_02_19_171951_create_car_requests_table.php
+++ database/migrations/2025_02_19_171951_create_car_requests_table.php
@@ -8,18 +8,27 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::table('telegram_users', function (Blueprint $table) {
+            $table->index('user_id');
+        });
+
         Schema::create('car_requests', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('telegram_users');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('telegram_users');
             $table->foreignId('car_id')->constrained('cars');
             $table->enum('status', ['created', 'in_progress', 'completed'])->default('created');
-            $table->json('data')->nullable(); // Для хранения всех данных формы в формате JSON
+            $table->json('data')->nullable();
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
+        Schema::table('telegram_users', function (Blueprint $table) {
+            $table->dropIndex('telegram_users_user_id_index');
+        });
+
         Schema::dropIfExists('car_requests');
     }
 };
